e85142044f464cfda2f91bc051fe63e02712daea
andrenanni <andre-nanni@hotmail.com>
2024-08-27 22:27:46 -0300

    ajuste no código

--- ProgramValidarSenha.cs
+++ ProgramValidarSenha.cs
@@ -5,33 +5,44 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
-         Validador validador = new Validador();
-
-        // Software de validação de senha
         Console.WriteLine("==== Informe uma senha ela deve conter Maiuscula e no minimo 8 caracteres. ====");
 
         Console.Write("Digite uma senha: ");
-        String senha = Console.ReadLine();
-        
-        Console.Write("Confirme a senha: ");
-        String confirmarSenha = Console.ReadLine();
+        string senha = ValidarSenha(Console.ReadLine());
 
-        // testa se todos foram positivos
-        if (validador.ValidarSenha(senha)) {
-            Console.WriteLine("Senha Valida!");
-        }else {
-            Console.WriteLine("Senha Invalida!");
-        }
-        // testa se a senha se coincidem
-        if (senha != confirmarSenha){
-            Console.WriteLine("Senhas nao se coincidem");
-        }
+        Console.Write("\nConfirme a senha: ");
+        string confirmaSenha = ValidarSenha(Console.ReadLine());
+
+        bool igual = ComparaTexto(senha, confirmaSenha);
+
+        if (igual)
+            Console.WriteLine("\nArmazenando dados!");
+        else
+            Console.WriteLine("\nRefaça o processo");
     }
-    // faz a validação 
-    private static string ValidarSenha(){
-        bool tamanhoValido = senha.Length >= 8;
-        bool contemNumero = senha.Any(char.IsDigit);
-        bool contemMaiuscula = senha.Any(char.IsUpper);
+
+    private static string ValidarSenha(string texto)
+    {
+        if (texto.Length < 8)
+            Console.WriteLine("Tamanho inválido");
+        if (!texto.Any(char.IsDigit))
+            Console.WriteLine("Não possui número!");
+        if (!texto.Any(char.IsUpper))
+            Console.WriteLine("Não possui letra maiúscula");
+        return texto;
+    }
+
+    private static bool ComparaTexto(string senha, string confirmaSenha)
+    {
+        if (senha == confirmaSenha)
+        {
+            Console.WriteLine("\nSenhas conferem!");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("\nSenhas não conferem!");
+            return false;
+        }
     }
 }
